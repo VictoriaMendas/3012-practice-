b2061f323593d177903e8a069ce430f4665035db
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2025-01-03 15:56:25 +0100

    new practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Counter from "./components/Counter/Counter";
 import CounterConfig from "./components/Counter/CounterConfig";
 
 function App() {
@@ -5,7 +6,8 @@ function App() {
     <>
       <h1>Practice 30.12.2024</h1>
       <hr />
-      <CounterConfig />
+      <Counter initialValue={5} step={3} />
+      {/* <CounterConfig /> */}
     </>
   );
 }
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,5 +1,28 @@
-const Counter = () => {
-  return <div>Counter</div>;
+import { useState } from "react";
+
+const Counter = ({ initialValue = 0, min = 0, max = 10, step = 1 }) => {
+  const [counter, setCounter] = useState(initialValue);
+  const increment = () => {
+    setCounter(counter + step);
+  };
+  const decrement = () => {
+    setCounter(counter - step);
+  };
+  const isDecrementDisabled = counter <= min;
+  const isIncrementDisabled = counter >= max;
+  return (
+    <div>
+      <button type="button" disabled={isDecrementDisabled} onClick={decrement}>
+        -
+      </button>
+      <span>{counter}</span>
+      <button type="button" disabled={isIncrementDisabled} onClick={increment}>
+        +
+      </button>
+    </div>
+  );
 };
 
 export default Counter;
+// Обмежити мождивість зміни стейту каунтер при умовах досягнення лічильника крайніх значень(ьін ьіч)
+// Зробити так щоб знаення лічильника не виходило за крайне значення
